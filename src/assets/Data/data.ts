cc2f408698ea105fca7e9a0d46be01328a31e3b6
Aleena Susan Biju <aleenasusan20@gmail.com>
2023-02-16 06:14:07 -0800

    assignment submitted

---
+++ src/assets/Data/data.ts
@@ -0,0 +1,110 @@
+export const districts=
+    [
+
+
+        {
+          "id": 1,
+          "title": "Trivandrum",
+          "details": "capital",
+          "Image": "https://www.happytrips.com/photo/57891680.cms"
+        },
+        {
+          "id": 2,
+          "title": "Kollam",
+          "details": "erstwhile Quilon",
+          "Image": "https://i.ytimg.com/vi/09ZfEAUAQUk/sddefault.jpg",
+        },
+        {
+           "id": 3,
+          "title": "Pathanamthitta",
+          "details": "Eye of Art",
+          "Image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf0GzYc9rBoc85pO9VHslAzJPtipYCSDZZ_cDn3kDdG5euPGLuUc0Zi7y2U_25-DgwKMs&usqp=CAU",
+        },
+    
+        
+          {
+            "id": 4,
+           "title": "Kottayam",
+           "details": "Aksharanagari",
+           "Image": "https://i.ytimg.com/vi/gXf83V-Cymw/maxresdefault.jpg"
+         },
+    
+         {
+          "id": 5,
+         "title": "Kannur",
+         "details": "Land of looms and lores",
+         "Image": "https://dtpckannur.com/public/img/inner/kannur.jpg"
+       },
+    
+       {
+        "id": 6,
+       "title": "Kasargode",
+       "details": "The land of gods",
+       "Image": "https://www.nativeplanet.com/img/2015/10/01-1443700764-bekalforte.jpg"
+       },
+     {
+      "id": 7,
+     "title": "Kozhikode",
+     "details": "City of Spices ",
+     "Image": "https://img.onmanorama.com/content/dam/mm/en/travel/travel-news/images/2021/7/1/kozhikode-beach-main.jpg"
+    },
+    
+    
+    {
+      "id": 8,
+     "title": "Malapuram",
+     "details": "City of Hills, Soccer",
+     "Image": "https://www.keralatourism.org/images/malabar/static-banner/large/Kodikuthimala-13012020091251.jpg"
+    },
+    
+    {
+      "id": 9,
+     "title": "Wayanad",
+     "details": "Wildlife Sanctuary ",
+     "Image": "https://assets.cntraveller.in/photos/60ba179da1a415b43b10bb90/master/pass/lead.jpg"
+    },
+    
+    {
+      "id": 10,
+     "title": "Thrissur",
+     "details": " The land of Poorams",
+     "Image": "https://www.keralatourism.org/images/festivals/large/20170330115924_321_1.jpg"
+    },
+    
+    
+    {
+      "id": 11,
+     "title": "Alapuzha",
+     "details": "'Venice of the East",
+     "Image": "https://upload.wikimedia.org/wikipedia/commons/e/e4/Alappuzha_Boat_Beauty_W.jpg"
+    },
+    
+    
+    {
+      "id": 12,
+     "title": "Ernakulam",
+     "details": " sea port",
+     "Image": "https://upload.wikimedia.org/wikipedia/commons/4/4a/INS_Vikrant_under_construction_at_Cochin_Shipyard.png"
+    },
+    
+    
+    {
+      "id": 13,
+     "title": "Palakkad",
+     "details": "The granary of Kerala",
+     "Image": "https://upload.wikimedia.org/wikipedia/commons/b/b0/Palakkad_Fort_5.jpg"
+    },
+    
+    
+    {
+      "id": 14,
+     "title": "Idukki",
+     "details": "rich flora and fauna",
+     "Image": "https://keralatourism.travel/images/destinations/headers/idukki-kerala-tourism-entry-fee-timings-holidays-reviews-header.jpg"
+    },
+    
+  ]
+    
+    
+    
+  
